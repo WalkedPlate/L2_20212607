b138fce059099dab779a6a0eb74c615ff341c9c9
Arkel <144744565+WalkedPlate@users.noreply.github.com>
2024-04-11 21:58:57 -0500

    ultimos cambios

--- src/main/java/com/example/l2_gtics/controller/GameController.java
+++ src/main/java/com/example/l2_gtics/controller/GameController.java
@@ -1,2 +1,71 @@
-package com.example.l2_gtics.controller;public class GameController {
+package com.example.l2_gtics.controller;
+
+
+import com.example.l2_gtics.entity.Parametros;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.apache.commons.lang3.Range;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Controller
+public class GameController {
+
+
+
+    @GetMapping(value ={"/buscaminas","","/"})
+    public String configurarBuscaminas(){
+        return "buscaminas";
+    }
+
+    @PostMapping(value = {"/buscaminas/guardar","/guardar","guardar"} )
+    public String guardarParametros(Model model, Parametros parametros){
+        model.addAttribute("parametros",parametros);
+
+        System.out.println(parametros.getFilas());
+        System.out.println(parametros.getColumnas());
+
+
+        return "redirect:/jugar";
+    }
+
+
+    @GetMapping(value ={"/jugar"})
+    public String jugarBuscaminas(Model model){
+
+        model.addAttribute("funciona","este es un mensaje");
+        ArrayList<Integer> listaFilas = new ArrayList<>();
+        for(int i=0;i< 5;i=i+1){
+            listaFilas.add(i);
+        }
+        model.addAttribute("listaFilas",listaFilas);
+
+        ArrayList<Integer> listaColumnas = new ArrayList<>();
+        for(int i=0;i< 6;i=i+1){
+            listaColumnas.add(i);
+        }
+        model.addAttribute("listaColumnas",listaColumnas);
+
+        return "jugar";
+    }
+
+    @PostMapping(value = {"/jugar/guardarcoordenada"} )
+    public String guardarCoordenada(Model model, @RequestParam("coordenada") String coordenada){
+
+        String[] partes = coordenada.split(" ");
+        int fila = Integer.parseInt(partes[0]);
+        int columna = Integer.parseInt(partes[1]);
+
+        model.addAttribute("filaExplotar",fila);
+        model.addAttribute("columnaExplotar",columna);
+
+
+
+        return "redirect:/jugar";
+    }
+
 }
--- src/main/java/com/example/l2_gtics/entity/Parametros.java
+++ src/main/java/com/example/l2_gtics/entity/Parametros.java
@@ -1,2 +1,19 @@
-package com.example.l2_gtics.entity;public class Parametros {
+package com.example.l2_gtics.entity;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.Scanner;
+
+@Getter
+@Setter
+
+public class Parametros {
+
+    private int filas;
+    private int columnas;
+    private int intentos;
+    private int bombas;
+    private String posiciones;
+
 }
